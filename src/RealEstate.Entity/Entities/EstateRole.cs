6f91fc78720a2f5a780c8e19c1d673ec68f1087f
ErenKaya1 <wtcerberus@gmail.com>
2021-02-17 03:50:46 +0300

    modified user columns

--- src/RealEstate.Entity/Entities/EstateRole.cs
+++ src/RealEstate.Entity/Entities/EstateRole.cs
@@ -1,8 +1,10 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace src.RealEstate.Entity.Entities
 {
+    [Table("role")]
     public class EstateRole : IdentityRole<Guid>
     {
         
--- src/RealEstate.Entity/Entities/EstateUser.cs
+++ src/RealEstate.Entity/Entities/EstateUser.cs
@@ -1,10 +1,52 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace src.RealEstate.Entity.Entities
 {
+    [Table("user")]
     public class EstateUser : IdentityUser<Guid>
     {
-        
+        [Required]
+        [StringLength(12)]
+        [Column(TypeName = "varchar(12)")]
+        public override string UserName { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        [Column(TypeName = "varchar(12)")]
+        public override string NormalizedUserName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column(TypeName = "varchar(100)")]
+        public override string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column(TypeName = "varchar(100)")]
+        public override string NormalizedEmail { get; set; }
+
+        [Required]
+        public override string PasswordHash { get; set; }
+
+        [NotMapped]
+        public override int AccessFailedCount { get; set; }
+
+        [NotMapped]
+        public override bool LockoutEnabled { get; set; }
+
+        [NotMapped]
+        public override DateTimeOffset? LockoutEnd { get; set; }
+
+        [NotMapped]
+        public override string PhoneNumber { get; set; }
+
+        [NotMapped]
+        public override bool PhoneNumberConfirmed { get; set; }
+
+        [NotMapped]
+        public override bool TwoFactorEnabled { get; set; }
     }
 }
