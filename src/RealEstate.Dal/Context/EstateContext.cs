f742f6f1789811e7f676616b26c799008e9c1b0a
ErenKaya1 <wtcerberus@gmail.com>
2021-02-17 05:12:42 +0300

    added console logger for entity framework core

--- src/RealEstate.Dal/Context/EstateContext.cs
+++ src/RealEstate.Dal/Context/EstateContext.cs
@@ -1,15 +1,24 @@
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using src.RealEstate.Entity.Entities;
 
 namespace src.RealEstate.Dal.Context
 {
     public class EstateContext : IdentityDbContext<EstateUser, EstateRole, Guid>
     {
+        private readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
         public EstateContext(DbContextOptions<EstateContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
