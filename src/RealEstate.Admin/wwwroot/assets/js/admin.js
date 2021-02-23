eb2fc0e049a22affa0cde9a12519102e87fd79e5
ErenKaya1 <wtcerberus@gmail.com>
2021-02-23 14:13:37 +0300

    added client side validation for new warming way form

--- src/RealEstate.Admin/wwwroot/assets/js/admin.js
+++ src/RealEstate.Admin/wwwroot/assets/js/admin.js
@@ -244,6 +244,23 @@ $(document).ready(function () {
     }
   });
 
+  $(".warmingway-form").on("submit", function (e) {
+    if (
+      $("input[name='WarmingWayNameTR']").val() == "" ||
+      $("input[name='WarmingWayNameEN']").val() == ""
+    ) {
+      e.preventDefault();
+      toastr["error"]("Lütfen tüm alanları doldurunuz.", "", {
+        positionClass: "toast-top-right",
+        closeButton: true,
+        progressBar: true,
+        preventDuplicates: true,
+        newestOnTop: true,
+        rtl: $("body").attr("dir") === "rtl" || $("html").attr("dir") === "rtl",
+      });
+    }
+  });
+
   var activeSidenav = $(
     ".sidenav-link[href='" + window.location.pathname + "']"
   ).parent();
